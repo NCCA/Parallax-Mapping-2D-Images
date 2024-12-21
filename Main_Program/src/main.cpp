77ae3f733bb344f443688be1053cd24cc81a0694
Omar Fayed <70962709+ofayed@users.noreply.github.com>
2024-12-21 20:45:58 +0000

    Update main.cpp

--- Main_Program/src/main.cpp
+++ Main_Program/src/main.cpp
@@ -5,21 +5,26 @@
 #include <fstream>
 #include <vector>
 #include "Image.h"
+#include "ImageData.h"
+#include <windows.h>
 
 using json = nlohmann::json;
-
 
 int main() {
 
 
      // Path to the JSON file containing the depth map
-    std::string json_file = "JSON_files/20241121_111331369_iOS.json";
- 
-    
+    std::string json_file = "assets/JSON_files/20241121_111331369_iOS.png";
+   
+    // Parse the JSON file into a nlohmann::json object
+    json j;
+
     Image depth(json_file);
-    depth.readDepthMap();
-    
-    depth.displayDepthMap();
+    // depth.readDepthMap(); 
+    // depth.displayDepthMap();
+
+    depth.readDepthNormals();
+    depth.displayDepthNormals();
     
   
     return 0;
